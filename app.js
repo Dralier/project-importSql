d811ce8d552f2f2c23cfd76dd9e309544dfc6a0f
Altairseven <elpiole@gmail.com>
2022-03-17 00:25:57 -0300

    asd

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const fileupload = require("express-fileupload");
-
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 5000;
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
 // Default
 app.use(
@@ -26,26 +30,77 @@ app.get("", (req, res) => {
 });
 
 app.post("", (req, res) => {
-    let sampleFile;
+    let sampleFile = req.files.sampleFile;
     let uploadPath;
 
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send("No se subio ningun archivo");
     }
 
-    //El nombre del input es sampleFile
-    sampleFile = req.files.sampleFile;
     uploadPath = __dirname + '/upload/' + sampleFile.name;
-    console.log(sampleFile);
 
-    //Usar mv() para poner un archivo en el servidor
-    sampleFile.mv(uploadPath, function(err){
+
+
+
+    
+    sampleFile.mv(uploadPath, async function(err){
+        //primer callback
+
         if(err) return res.status(500).send(err);
+        const fileStream = fs.createReadStream(uploadPath);
+          
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+        // Note: we use the crlfDelay option to recognize all instances of CR LF
+        // ('\r\n') in input.txt as a single line break.
+        
+        for await (const line of rl) {
+            // Each line in input.txt will be successively available here as `line`.
+            console.log(`Line from file: ${line}`);
 
-        res.send('Archivo subido');
+            //aca podes insertar renglon por renglon directo en la db, o meterlo en un array y despues llamar a una funcion que hace la escritura.
+            //para que quede mas ordenado
+
+            nomeacuerdoComoEs.query("insert into asdasd", ()=> {
+                //otra callback
+
+
+            })
+
+        }
 
     });
 
+
+
+    // let sampleFile;
+    // let uploadPath;
+
+    // if (!req.files || Object.keys(req.files).length === 0) {
+    //     return res.status(400).send("No se subio ningun archivo");
+    // }
+
+    //El nombre del input es sampleFile
+    // sampleFile = req.files.sampleFile;
+
+    // var asd = fs.readFile(sampleFile.data.buffer,(fd)=> console.log(fd));
+
+
+
+    // uploadPath = __dirname + '/upload/' + sampleFile.name;
+    // console.log(sampleFile);
+
+    // //Usar mv() para poner un archivo en el servidor
+    // sampleFile.mv(uploadPath, function(err){
+    //     if(err) return res.status(500).send(err);
+
+    //     res.send('Archivo subido');
+
+    // });
+
+   
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
